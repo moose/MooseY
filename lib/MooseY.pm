cc46502168553a87ea119bc55efbb59e0823e313
Karen Etheridge <ether@cpan.org>
2013-06-10 12:49:00 -0700

    initial commit, via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.007, github profile.

---
+++ lib/MooseY.pm
@@ -0,0 +1,53 @@
+use strict;
+use warnings;
+package MooseY;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use $name;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=over 4
+
+=item * C<foo>
+
+...
+
+=back
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=MooseY>
+(or L<bug-MooseY@rt.cpan.org|mailto:bug-MooseY@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=begin :list
+
+* L<foo>
+
+=end :list
+
+=cut
